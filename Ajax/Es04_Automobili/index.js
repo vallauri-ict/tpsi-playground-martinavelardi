c05205777212afec9b89036d2df35ff0fe207320
Stud. Martina Velardi <m.velardi.1394@vallauri.net>
2021-02-03 12:57:22 +0100

    Es4_Automobili

--- Ajax/Es04_Automobili/index.js
+++ Ajax/Es04_Automobili/index.js
@@ -8,7 +8,7 @@ let intestazioni = [{ "tag": "th", "text": "nomeModello", "width": "15%" },
     { "tag": "th", "text": "anno", "width": "10%" },
     { "tag": "th", "text": "img", "width": "20%" },
     { "tag": "th", "text": "Dettagli", "width": "13%" },
-    { "tag": "th", "text": "elimina", "width": "12%" }
+    { "tag": "th", "text": "Elimina", "width": "12%" }
 ];
 
 
@@ -52,8 +52,11 @@ $(document).ready(function() {
     });
 
     _lstModelli.on("change", function() {
+        let opzioneSelezionata = _lstModelli.children("option").eq(_lstModelli.prop("selectedIndex")).text(); // puntatore jquery perché c'è .eq()
+        _lstModelli.prop("nome", opzioneSelezionata.split(' - ')[0]);
+        _lstModelli.prop("alimentazione", opzioneSelezionata.split(' - ')[1]);
         let codiceModello = _lstModelli.val();
-        let request = inviaRichiesta("get", URL + "/modelli?codMarca=" + codiceMarca);
+        let request = inviaRichiesta("get", URL + "/modelli?codMarca=" + codiceModello);
         request.fail(errore);
         request.done(function(automobili) {
             let thead = $("<thead>");
@@ -66,6 +69,46 @@ $(document).ready(function() {
                 th.appendTo(tr);
                 th.text(intestazioni[i].text);
                 th.css({ "width": intestazioni[i].width });
+            }
+            let tbody = $("<tbody");
+            tbody.appendTo(_table);
+            for (const automobile of automobili) {
+                let tr = $("<tr>")
+                tr.appendTo(tbody)
+                let td = $("<td>")
+                td.appendTo(tr)
+                td.text(_lstModelli.prop("nome"))
+
+                td = $("<td>")
+                td.appendTo(tr)
+                td.text(_lstModelli.prop("alimentazione"))
+
+                td = $("<td>")
+                td.appendTo(tr)
+                td.text(automobile.colore)
+
+                td = $("<td>")
+                td.appendTo(tr)
+                td.text(automobile.anno)
+
+                td = $("<td>")
+                td.appendTo(tr)
+                let img = $("<img>")
+                img.appendTo(td)
+                img.prop("src", "img/" + automobile.img)
+                img.css({ "height": "65px" })
+
+                td = $("<td>")
+                td.appendTo(tr)
+                let button = $("<button>")
+                button.appendTo(td)
+                td.text("Dettagli")
+
+                td = $("<td>")
+                td.appendTo(tr)
+                let button = $("<button>")
+                button.appendTo(td)
+                td.text("Elimina")
             }
         });
 
