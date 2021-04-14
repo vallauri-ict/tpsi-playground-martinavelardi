e3caf031eb6b6b8291ca2acb45f7b8ea7365ae18
Martina Velardi <m.velardi.1394@vallauri.edu>
2021-04-14 13:03:32 +0200

    Terminato Es02. Iniziato Es03

--- PHP/Es02_Maggiore_Tra_Numeri/pagina2.php
+++ PHP/Es02_Maggiore_Tra_Numeri/pagina2.php
@@ -14,10 +14,41 @@
 
 <body>
     <h1>Grazie della tua richiesta</h1>
+
+    <!-- PHP -->
     <?php
-    # todo
+    $n1 = 0;
+    $n2 = 0;
+    $n3 = 0;
+
+    # Controllo parametri
+    if (isset($_REQUEST["n1"]) && is_numeric($_REQUEST["n1"])) {
+        $n1 = $_REQUEST["n1"];
+    } else
+        die("Primo numero non valido");
+    if (isset($_REQUEST["n2"]) && is_numeric($_REQUEST["n2"])) {
+        $n2 = $_REQUEST["n2"];
+    } else
+        die("Secondo numero non valido");
+
+    if (isset($_REQUEST["n3"]) && is_numeric($_REQUEST["n3"])) {
+        $n3 = $_REQUEST["n3"];
+    } else
+        die("Terzo numero non valido");
+
+    # Controllo del numero
+    if ($n1 > $n2 && $n1 > $n3) {
+        echo ("Il numero maggiore è il primo e vale $n1");
+    } else if ($n2 > $n3) {
+        echo ("Il numero maggiore è il secondo e vale $n2");
+    } else {
+        echo ("Il numero maggiore è il terzo e vale $n3");
+    }
+    echo ("<br>Tipo di richiesta: <b>$_SERVER[REQUEST_METHOD]</b><br>");
+    $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    echo ("URL richiedente: $actual_link");
     ?>
-    <h2>Il numero maggiore tra quelli ricevuti è $</h2>
+
 </body>
 
 </html>
---
+++ PHP/Es03_Registrazione/index.js
@@ -0,0 +1,23 @@
+"use strict"
+$(document).ready(function () {
+    $("select[name=lstCitta]").prop("selectedIndex", -1)
+    $("#btnInvia").on("click", function () {
+        let msg = ""
+        if ($("#txtNome").val() == "") {
+            msg += "Nome mancante<br>"
+        }
+        if ($("input[name=optIndirizzo]:checked").length == 0) {
+            msg += "Indirizzo di studio non selezionato<br>"
+        }
+        if (msg != "") {
+            $("#msg").html(msg)
+        }
+        else {
+            $("#msg").html("")
+            let form = $("#form1")
+            form.prop("action", "pagina2.php")
+            form.prop("method", "get")
+            form.submit()
+        }
+    })
+})
